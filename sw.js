1eea857a7bfa57881990be20186f42393304c403
malloyruiz <info@malloyruiz.com>
2026-03-25 16:43:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,9 +3,10 @@ const ASSETS = [
     './',
     './index.html',
     './manifest.json',
-    './icon.png',
     './icon-192.png',
     './icon-512.png',
+    './icon-1024.png',
+    './screenshot1.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
     'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&family=Inter:wght@200;300;400;500;600&display=swap'
